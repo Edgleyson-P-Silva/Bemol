95d72584a84ab4a6c227ac159bc76e47fe3daa9f
Edgleyson <eps5@cin.ufpe.br>
2024-08-20 18:00:03 -0300

    Variaveis pelo inspecionar elemento pra ver se assim funciona

--- etapa3_busca_hotel_page.py
+++ etapa3_busca_hotel_page.py
@@ -7,5 +7,13 @@ class BuscaHotelTrivago:
         self.driver = driver # para  intereção como navegador
 
     # Localizar elementos na página
-    campo_busca = (By.CSS_SELECTOR, "input[placeholder='Para onde?']")
+    campo_busca = (By.CSS_SELECTOR, "input[data-testid='search-form-destination']")
+    botao_pesquisar = (By.CSS_SELECTOR, "[data-testid='search-button-with-loader']")
+    botao_ordenar = (By.CSS_SELECTOR, "[data-testid='sorting-selector-select']")
+    opcao_avaliacao_sugestao = (By.CSS_SELECTOR, "option[value='6']")
+    primeiro_resultado = (By.CSS_SELECTOR, "button[data-testid='item-name']")
+    avaliacao_estrela = (By.CSS_SELECTOR, "meta[itemprop='ratingValue']")
+    avaliacao_nota = (By.CSS_SELECTOR, "meta[itemprop='ratingValue']")
+    preco = (By.CSS_SELECTOR, "div[data-testid='expected-price'] span b")
+
   
