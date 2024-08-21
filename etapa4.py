71fd9de91fe719fe580e5e0226cd9d18a7116e6d
Edgleyson <eps5@cin.ufpe.br>
2024-08-20 21:42:52 -0300

    novo metodo

--- etapa4.py
+++ etapa4.py
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-import unittest 
-import requests #requisicoes http para ineragir com a api
-
-class TestServerestAPI(unittest.TestCase):
-
-    def setUp(self):
-        self.url_base = "https://serverest.dev"
-
-    def criar_usuario(self):
-        novo_usuario = {
-            "nome": "Edgleyson Silva",
-            "email": "edgleyson99@outlook.com",
-            "password": "qaenginnerbemol2024",
-            "administrador": "true"
-        }
-
-    def login(self):
-        dados_login = {
-            "email": "edgleyson99@outlook.com", 
-            "password": "qaenginnerbemol2024"
-        }
-=======
 import requests #requisicoes http para ineragir com a api
 
 url_base = "https://serverest.dev"
@@ -33,4 +10,3 @@ usuario = {
     "administrador": "true"  # Define se o usuário é um administrador
 }
 
->>>>>>> Stashed changes
