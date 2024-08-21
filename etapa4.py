10bfc4b7e30bbadc69e5ce57968891811630f568
Edgleyson <eps5@cin.ufpe.br>
2024-08-20 21:41:31 -0300

    unittest nao pegando

--- etapa4.py
+++ etapa4.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import unittest 
 import requests #requisicoes http para ineragir com a api
 
@@ -19,3 +20,17 @@ class TestServerestAPI(unittest.TestCase):
             "email": "edgleyson99@outlook.com", 
             "password": "qaenginnerbemol2024"
         }
+=======
+import requests #requisicoes http para ineragir com a api
+
+url_base = "https://serverest.dev"
+headers = HEADERS = {"Content-Type": "application/json"}  # Cabeçalhos HTTP em json
+
+usuario = {
+    "nome": "Edgleyson Silva",  # Nome do usuário a ser criado
+    "email": "edgleyson99@outlook.com",  # Email do usuário a ser criado
+    "password": "qaenginnerbemol2024",  # Senha do usuário a ser criada
+    "administrador": "true"  # Define se o usuário é um administrador
+}
+
+>>>>>>> Stashed changes
