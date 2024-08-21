61e9d46cde3ad01ce220011d012b8c2b1489cdb3
Edgleyson <eps5@cin.ufpe.br>
2024-08-20 21:18:06 -0300

    usuario

---
+++ etapa4.py
@@ -0,0 +1,15 @@
+import unittest 
+import requests #requisicoes http para ineragir com a api
+
+class TestServerestAPI(unittest.TestCase):
+
+    def setUp(self):
+        self.url_base = "https://serverest.dev"
+
+    def criar_usuario(self):
+        novo_usuario = {
+            "nome": "Edgleyson Silva",
+            "email": "edgleyson99@outlook.com",
+            "password": "qaenginnerbemol2024",
+            "administrador": "true"
+        }
