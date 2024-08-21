b741bf3765735ec47c6cc227160e8f3a1a990253
Edgleyson <eps5@cin.ufpe.br>
2024-08-20 22:16:57 -0300

    criaer e buscar produto

--- etapa4.py
+++ etapa4.py
@@ -54,3 +54,40 @@ def verificar_usuario(usuario_id):
     else:
         # Levanta uma exceção se a requisição falhar, com uma mensagem de erro
         raise Exception("Erro ao buscar o usuário.")
+    
+def criar_produto():
+
+    # Criar um novo produto na API
+    response = requests.post(f"{url_base}/produtos", json=produto, headers=HEADERS)
+    
+    # Imprime o status
+    print(f"Criar produto: {response.status_code}, {response.text}")
+    
+    # Verifica se foi bem-sucedida (código 201 Created)
+    if response.status_code == 201:
+        # Retorna o ID do produto criado a partir da resposta
+        return response.json().get('id')
+    else:
+        # Levanta uma exceção se a requisição falhar, com uma mensagem
+        raise Exception("Erro ao criar o produto.")
+
+def verificar_produto(produto_id):
+
+    # buscar o produto pelo ID na API
+    response = requests.get(f"{url_base}/produtos/{produto_id}", headers=HEADERS)
+    
+    print(f"Verify Product Response: {response.status_code}, {response.text}")
+    
+    # (código 200 OK)
+    if response.status_code == 200:
+        # Dsdos do produto da resposta JSON
+        product_data = response.json()
+        
+        # Verifica o nome
+        assert product_data['nome'] == produto['nome'], "Nome do produto não corresponde."
+        
+        # Verifica preço
+        assert product_data['preco'] == produto['preco'], "Preço do produto não corresponde."
+    else:
+        # Se falahr mensagem de erro
+        raise Exception("Erro ao buscar o produto.")
